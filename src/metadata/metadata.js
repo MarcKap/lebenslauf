682741e89c1c6fb16944760120b7f529efef116e
Marco <serienjunkie69@gmil.com>
2022-02-09 14:52:21 +0100

    added all contant

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: "Kappeller Marco",
+  name: "Marco Kappeller",
   title: "Junior Frontend Developer",
   facts: {
     Wohnort: {
@@ -16,11 +16,11 @@ module.exports = {
     },
   },
   techskills: [
-    ["React", "+++"],
-    ["React-Native", "++++"],
+    ["React", "++++"],
+    ["React-Native", "+++++"],
     ["Redux", "+++"],
-    ["JavaScript", "+++"],
-    ["TypeScript", "++"],
+    ["JavaScript", "++++"],
+    ["TypeScript", "+++"],
     ["HTML5", "++"],
     ["CSS3", "+++"],
     ["Ergonomie", "++++"],
@@ -28,6 +28,7 @@ module.exports = {
   methodskills: [
     ["Scrum", "+++"],
     ["Kanban", "++++"],
+    ["Statistik", "+++"],
   ],
   education: [
     {
@@ -60,25 +61,56 @@ module.exports = {
       header: "Corona-App Daicy",
       skills: ["React", "React-Native", "Redux", "Ergonomie", "TypeScript"],
       contents:
-        "craftbeer cycling bananas furry foodorder commanding spiderweb indigo",
+        "Entwicklung einer App zur Unterbrechung von Infektionsketten auf Basis von React-Native für den Main-Kinzig-Kreis. Ein auf React basierendes Backend empfängt Antigentest-Ergebnisse aus der App und dient zur Verwaltung von Testterminen. Über die GraphQL Schnittstelle können auch Nachrichten aus dem Backend in die App gepusht werden. Bei Bedarf können die Begegnungen aus dem Kalender im pdf Format exportiert werden.",
+      tasks: [
+        "Überarbeiten des Fontends damit die rechtlichen Kriterien für Barrierefreiheit eingehalten werden",
+        "Rolle bzw Rechte für Testzentren im Backend ",
+        "Zertifikate um Impfzertifikate erweitern",
+        "Testen",
+        "Demonstration der neuen Features beim Kunden",
+        "Qualitätssicherung",
+      ],
+      duration: "9 Monate",
+      scope: ["4 Frontend-Entwickler", "1 Backend-Entwickler", "1 UI-Designer"],
     },
     {
       header: "Michel CNC Stellenangebote",
       skills: ["Wordpress", "HTML"],
       contents:
-        "craftbeer cycling bananas furry foodorder commanding spiderweb indigo",
+        "Erstellen eines neuen Bereichs für Stellenangebote in einer bereits Vorhandenen Webseite, die mit Wordpress erstellt wurde ",
+      tasks: [
+        "Eigenverantwortliches Umsetzen der Vorgaben",
+        "Kommunikation mit dem Kunden",
+      ],
+      duration: "1 Woche",
+      scope: ["1 Frontend-Entwickler"],
     },
     {
       header: "Werbeseite für die App Daicy",
       skills: ["October CMS", "HTML", "CSS"],
       contents:
-        "craftbeer cycling bananas furry foodorder commanding spiderweb indigo",
+        "Bei der Webseite der App Daicy sollen das Aussehen und die Inhalte auf dem Stand mit der App gehalten werden. Außerdem musste bei den veralteten Plug-ins und dem October CMS ein Update gemacht werden  ",
+      tasks: [
+        "Eigenverantwortliches Anpassen des Layouts an die Änderungen in der App",
+        "Aufsetzen einer Erklärung zur Barrierefreiheit",
+        "Pflege der Inhalte",
+      ],
+      duration: "9 Monate",
+      scope: ["1 Frontend-Entwickler"],
     },
     {
       header: "Empirische Untersuchung zur Mitarbeiterzufriedenheit",
       skills: ["SPSS", "Statistik", "Projektplanung"],
       contents:
-        "craftbeer cycling bananas furry foodorder commanding spiderweb indigo",
+        "Durchführung und Auswertung einer Zufriedenheitsumfrage im Auftrag der Elkamet GmbH. Die Zufriedenheit der Mitarbeiter wurde in 4 Kategorien quantitativ geprüft. Die Ergebnisse sollten Rückschlüsse auf die von Elkamet einsetzten Arbeitsmethoden zulassen. Die Auswertung der gesammelten Daten erfolgte mit SPSS 22.",
+      tasks: [
+        "Design des Fragebogens",
+        "Schreiben eines R-Scripts um 'fehlende Werte' zu ersetzten",
+        "Datenauswertung",
+        "Graphisches Aufbereiten der Ergebnisse",
+      ],
+      duration: "6 Monate",
+      scope: ["4 Studentische Kräfte"],
     },
   ],
 };
